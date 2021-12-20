b42c6bcbdb0fd3eae2a24534a168dcf51a820ca6
genie <luvbliny@gmail.com>
2021-12-20 12:02:58 +0900

    feat : test init

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [state, setState] = useState([]);
+  const [count, setCount] = useState(-1);
+
+  useEffect(() => {}, state);
+
+  const onPlusHandler = () => {
+    const list = [...state, count + 1];
+    setState(list);
+    setCount((count) => count + 1);
+  };
+
+  const onMinusHandler = (e, index) => {
+    const a = state.indexOf(index);
+    const list = state.filter((i) => i !== a);
+    console.log(state);
+    console.log(list);
+    console.log(index);
+    console.log(a);
+    setState([...list]);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +31,15 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={onPlusHandler}>+</button>
+        <div>
+          {" "}
+          {state.map((item, index) => (
+            <button key={index} onClick={(e) => onMinusHandler(e, index)}>
+              {item}
+            </button>
+          ))}{" "}
+        </div>
       </header>
     </div>
   );
